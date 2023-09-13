7fcb9fcf674fb38e40d8b3cf52ff32810b04ff9b
Felipe Fraxe <felipe_fraxe@hotmail.com>
2023-09-13 08:31:50 -0400

    Questoes 3 e 5

---
+++ prova01/questao03/script.js
@@ -0,0 +1,7 @@
+(() => {
+    document.querySelector(".soma").addEventListener("click", () => {
+        const num1 = parseInt(prompt("Digite um número"));
+        const num2 = parseInt(prompt("Digite outro número"));
+        document.querySelector(".resultado").innerHTML = `A soma entre ${num1} e ${num2} é ${num1 + num2}`;
+    });
+})()
