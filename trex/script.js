4c57256a67b11d78f587ff4f04020d3e88274433
Felipe Fraxe <felipe_fraxe@hotmail.com>
2023-09-26 21:57:00 -0400

    Some more functionalities

--- trex/script.js
+++ trex/script.js
@@ -1,37 +1,23 @@
 (() => {
 
     const FPS = 300;
-    const HEIGHT = 300;
     const WIDTH = 1024;
     const PROB_NUVEM = 0.2;
-    const PROB_CACTO = 0.1;
-  
+    const PROB_CACTO = 0.5;
+    
+    let score_board_digits = 5;
     let gameLoop;
-    let deserto;
-    let dino;
     let frame = 0;
-    let nuvens = [];
-    let cactos = [];
-
-    function init() {
-        gameLoop = setInterval(run, 1000 / FPS);
-        deserto = new Deserto();
-        dino = new Dino();
-    }
-
-    window.addEventListener("keydown", (e) => {
-        if(e.code === "Space") {
-            if(dino.status === 0)
-                dino.status = 1;
-        }
-    });
+    const nuvens = [];
+    const cactos = [];
+    const scoreBoard = [];
+    let paused = false;
+    let started = false;
 
     class Deserto {
         constructor() {
             this.element = document.createElement("div")
             this.element.className = "deserto";
-            this.element.style.width = `${WIDTH}px`;
-            this.element.style.height = `${HEIGHT}px`;
             document.getElementById("game").appendChild(this.element);
   
             this.chao = document.createElement("div");
@@ -49,26 +35,29 @@
         #status
         constructor() {
             this.backgroundPositionsX = {
+                parado: "-1256px",
                 correndo1: "-1391px",
                 correndo2: "-1457px",
-                pulando: "-1259px"
+                pulando: "-1259px",
+                morto: "-1522px",
+                agachado1: "-1651px",
+                agachado2: "-1740px"
             }
-            this.#status = 0; // 0-correndo, 1-subindo, 2-descendo
+            this.#status = 0; // 0-correndo, 1-subindo, 2-descendo, 3-agachado
             this.altumaMinima = 2;
             this.altumaMaxima = 100;
             this.element = document.createElement("div")
             this.element.className = "dino";
-            this.element.style.backgroundPositionX = this.backgroundPositionsX.correndo1;
+            this.element.style.backgroundPositionX = this.backgroundPositionsX.parado;
             this.element.style.backgroundPositionY = "-2px";
             this.element.style.bottom = `${this.altumaMinima}px`;
+            this.element.style.height = "70px";
+            this.element.style.width = "66px";
             deserto.element.appendChild(this.element);
         }
-        /**
-        * @param {number} value
-        */
 
         set status(value) {
-            if(value >= 0 && value <= 2)
+            if(value >= 0 && value <= 3)
                 this.#status = value;
         }
         
@@ -77,18 +66,19 @@
         }
         
         correr() {
-            if(this.#status === 0)
+            if(this.#status === 0) {
                 this.element.style.backgroundPositionX = this.element.style.backgroundPositionX === this.backgroundPositionsX.correndo1 ? this.backgroundPositionsX.correndo2 : this.backgroundPositionsX.correndo1;
-            else if(this.#status === 1) {
+            } else if(this.#status === 1) {
                 this.element.style.backgroundPositionX = this.backgroundPositionsX.pulando;
                 this.element.style.bottom = `${parseInt(this.element.style.bottom) + 1}px`;
                 if(parseInt(this.element.style.bottom) >= this.altumaMaxima)
                     this.status = 2;
-            }
-            else if(this.#status === 2) {
+            } else if(this.#status === 2) {
                 this.element.style.bottom = `${parseInt(this.element.style.bottom) - 1}px`;
                 if(parseInt(this.element.style.bottom) <= this.altumaMinima)
                     this.status = 0;
+            } else if(this.#status === 3) {
+                this.element.style.backgroundPositionX = this.element.style.backgroundPositionX === this.backgroundPositionsX.agachado1 ? this.backgroundPositionsX.agachado2 : this.backgroundPositionsX.agachado1;
             }
         }
     }
@@ -120,41 +110,171 @@
         }
     }
 
+    class Score {
+        constructor() {
+            this.element = document.createElement("div");
+            this.element.className = "score";
+            deserto.element.appendChild(this.element);
+            this.element.style.backgroundPositionX = "-970px";
+        }
+
+        increment() {
+            if(this.element.style.backgroundPositionX != "-1105px") {
+                this.element.style.backgroundPositionX = `${parseInt(this.element.style.backgroundPositionX) - 15}px`;
+                return false;
+            } else {
+                this.element.style.backgroundPositionX = "-970px";
+                return true;
+            }
+        }
+
+        reset() {
+            this.element.style.backgroundPositionX = "-970px";
+        }
+    }
+
+    const deserto = new Deserto();
+    const dino = new Dino();
+    for(let i = 0; i < score_board_digits; i++) {
+        scoreBoard.push(new Score());
+        scoreBoard[i].element.style.right = `${15 + (i * 15)}px`;
+    }
+
+    function init() {
+        gameLoop = setInterval(run, 1000 / FPS);
+    }
+    
+    function gameOver() {
+        dino.element.style.backgroundPositionX = dino.backgroundPositionsX.morto;
+        clearInterval(gameLoop);
+
+        const restartBtn = document.createElement("div");
+        restartBtn.className = "restartBtn";
+        const gameOverMsg = document.createElement("div");
+        gameOverMsg.className = "gameOver";
+        restartBtn.addEventListener("click", () => {
+            while(nuvens.length > 0) {
+                nuvens[0].element.remove();
+                nuvens.shift();
+            }
+
+            while(cactos.length > 0) {
+                cactos[0].element.remove();
+                cactos.shift();
+            }
+
+            for(let i = 0; i < score_board_digits; i++) {
+                scoreBoard[i].reset();
+            }
+            score_board_digits = 5;
+
+            dino.element.style.backgroundPositionX = dino.backgroundPositionsX.parado;
+            dino.element.style.width = "66px";
+            dino.element.style.height = "70px";
+            started = false;
+            restartBtn.remove();
+            gameOverMsg.remove();
+        });
+
+
+        deserto.element.appendChild(restartBtn);
+        deserto.element.appendChild(gameOverMsg);
+    }
+
+    window.addEventListener("keydown", ({ code, key }) => {
+        if(code === "Space") {
+            if(!started) {
+                started = true;
+                init();
+            } else if(!paused && dino.status === 0) {
+                dino.status = 1;
+            }
+        } else if(key === 'p') {
+            paused = !paused;
+        } else if(key === "ArrowDown") {
+            if(!paused && dino.status === 0) {
+                dino.status = 3;
+                dino.element.style.width = "90px";
+                dino.element.style.height = "45px";
+                dino.element.style.backgroundPositionY = "-27px";
+            }
+        } else if(key === "ArrowUp") {
+            if(!paused && dino.status !== 1) {
+                dino.status = dino.status === 0 ? 1 : 0;
+                dino.element.style.width = "66px";
+                dino.element.style.height = "70px";
+                dino.element.style.backgroundPositionY = "-2px";
+            }
+        }
+    });
+
+    window.addEventListener("keyup", ({ key }) => {
+        if(key === "ArrowDown") {
+            if(!paused && dino.status === 3) {
+                dino.status = 0;
+                dino.element.style.width = "66px";
+                dino.element.style.height = "70px";
+                dino.element.style.backgroundPositionY = "-2px";
+            }
+        }
+    })
+
     function run() {
+        if(paused)
+            return;
+
         frame = (frame + 1) % FPS;
 
         deserto.mover();
         dino.correr();
-        cactos = cactos.filter(cacto => {
-            cacto.mover();
-            const xPos = WIDTH - parseInt(cacto.element.style.right);
-            if(xPos === 40 && parseInt(dino.element.style.bottom) <= 60)
-                console.log("TOQUE");
-            return parseInt(cacto.element.style.right) < WIDTH;
-        });
-
 
         if(Math.random() * 100 <= PROB_NUVEM)
             nuvens.push(new Nuvem());
 
         if(Math.random() * 100 <= PROB_CACTO) {
             const cacto = new Cacto();
-            if(cactos.length === 0 || (parseInt(cacto.element.style.right) < parseInt(cactos[cactos.length - 1].element.style.right) - 170))
+            if(cactos.length === 0 || (parseInt(cacto.element.style.right) < parseInt(cactos[cactos.length - 1].element.style.right) - 170)) {
                 cactos.push(cacto);
+            } else {
+                cacto.element.remove();
+            }
         }
 
+        cactos.forEach(cacto => {
+            cacto.mover();
+    
+            const xPos = WIDTH - parseInt(cacto.element.style.right);
+            if(xPos === 40 && parseInt(dino.element.style.bottom) <= 60) {
+                gameOver();
+            }
+    
+            if(xPos < 0) {
+                cactos.shift();
+                cacto.element.remove();
+            }
+        });
+
+    
         if(frame % 2 === 0) {
-            nuvens = nuvens.filter(nuvem => {
+            nuvens.forEach(nuvem => {
                 nuvem.mover();
-                return parseInt(nuvem.element.style.right) < WIDTH;
+                if(parseInt(nuvem.element.style.right) > WIDTH) {
+                    nuvens.shift();
+                    nuvem.element.remove();
+                }
             });
         }
-    }
 
-    function gameOver() {
-        
+        if(frame % 30 === 0) {
+            let i = 0;
+            while(scoreBoard[i].increment()) {
+                i++;
+                if(i === score_board_digits) {
+                    scoreBoard.push(new Score());
+                    scoreBoard[i].element.style.right = `${15 + (i * 15)}px`;
+                    score_board_digits++;
+                }
+            }
+        }
     }
-
-    init();
-  
-})()
+})();
