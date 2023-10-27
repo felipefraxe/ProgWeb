b80c71a7f4b127ac258b0bd7f96f873de7d10e1f
Felipe Fraxe <felipe_fraxe@hotmail.com>
2023-10-27 18:47:34 -0400

    correcao debugs

--- src/controllers/main.js
+++
@@ -1,9 +0,0 @@
-function index(_req, res) {
-    res.render("main");
-}
-
-function about(_req, res) {
-    res.render("main/sobre");
-}
-
-module.exports = { index, about };
--- src/router/router.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const mainController = require("../controllers/main");
-const router = Router();
-
-router.get("/", mainController.index);
-router.get("/sobre", mainController.about);
-
-module.exports = router;
